bcf82a301387bb9994bdcff143285ff9747b7e8a
Karthik Rao <karthikrrao30@gmail.com>
2021-04-04 00:52:21 +0530

    modified branch_unit

--- src/branch_unit.v
+++ src/branch_unit.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "params.vh"
 
 module branch_unit(
     input [31:0] slt_out,
@@ -9,21 +10,14 @@ module branch_unit(
     output reg branch_taken
     );
     
-    parameter BEQ =3'b000;  
-    parameter BNE =3'b001; 
-    parameter BLT =3'b100; 
-    parameter BGE =3'b101; 
-    parameter BLTU= 3'b110; 
-    parameter BGEU= 3'b111;
-    
     always@(*) begin
         casex(instruction[14:12])
-            BEQ: begin branch_taken=(equal_out)&branch; end
-            BNE: begin branch_taken=(~equal_out)&branch; end
-            BLT: begin branch_taken=(slt_out)&branch; end 
-            BGE: begin branch_taken=(~slt_out)&branch; end 
-            BLTU: begin branch_taken=(sltu_out)&branch; end 
-            BGEU: begin branch_taken=(~sltu_out)&branch; end     
+            `BEQ: begin branch_taken=(equal_out)&branch; end
+            `BNE: begin branch_taken=(~equal_out)&branch; end
+            `BLT: begin branch_taken=(slt_out)&branch; end 
+            `BGE: begin branch_taken=(~slt_out)&branch; end 
+            `BLTU: begin branch_taken=(sltu_out)&branch; end 
+            `BGEU: begin branch_taken=(~sltu_out)&branch; end     
             default: branch_taken=0;    
         endcase
     end
